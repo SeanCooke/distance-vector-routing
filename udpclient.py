ac8c81f76ad2eafc9e8fcadc2dac951b35c7030e
Sean Cooke <scooke3@ms10.csug.rochester.edu>
2016-10-06 18:35:28 -0400

    Initial Commit.

---
+++ udpclient.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from socket import *
+
+serverName = 'ms10.csug.rochester.edu'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence: ')
+clientSocket.sendto(message,(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print modifiedMessage
+clientSocket.close()
