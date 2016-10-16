c2c55086cfab0fb8022dd350d66d946e6a8d7714
Cooke <scooke@smddevmysql01.urmc-sh.rochester.edu>
2016-10-16 19:12:10 -0400

    Testing...

--- dvr.py
+++ dvr.py
@@ -84,8 +84,9 @@ def updateRoutingTable(distanceVectorDictionaryReceived):
 	# hostRecieved will hold the host from which this distance vector was sent
 	hostRecieved = distanceVectorDictionaryReceived['host']
 	# Only listen to hosts to which route exists in [routingTable]
+	print "\n*****\n"
+	print "routingTable: "+str(routingTable)+"\n"
 	if hostRecieved in routingTable:
-		print "\n*****"
 		for distanceVectorHostRecieved, distanceVectorCostRecieved in distanceVectorDictionaryReceived.iteritems():
 			if distanceVectorHostRecieved != 'host' and distanceVectorHostRecieved != hostname:
 				# If the host [distanceVectorHostRecieved] exists in [routingTable], see
@@ -105,7 +106,7 @@ def updateRoutingTable(distanceVectorDictionaryReceived):
 					newCost = routingTable[hostRecieved]['cost'] + distanceVectorCostRecieved
 					newNextHop = routingTable[hostRecieved]['nextHop']
 					routingTable[distanceVectorHostRecieved] = {'nextHop':newNextHop, 'cost':newCost}
-		print "*****\n"
+	print "*****\n"
 
 class serverThread(threading.Thread):
 	def __init__(self, port):
