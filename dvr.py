484b47845a35f4415bfb3aee952e6ad1ef053668
Cooke <scooke@smddevmysql01.urmc-sh.rochester.edu>
2016-10-16 20:51:12 -0400

    Added comments.

--- dvr.py
+++ dvr.py
@@ -45,7 +45,24 @@ def computeDistanceVector(dataFileLocation):
 			line = dataFile.readline()
 			lineIndex += 1
 	return distanceVector
-	
+
+# initializeRoutingTable takes a distance vector and returns
+# routing table based on the information in the distance vector.
+#
+# input arguments:
+# 1. distanceVector - A dictionary containing name/value pairs
+# where each key is the string name of a directly connected host
+# and each value is the float cost of the link between this host
+# and the key.  The key/value pair "host' = [gethostname()]" must
+# be included in  [distanceVector].
+#
+# return values:
+# 1. routingTable - A dictionary containing name value pairs where
+# each key is the string name of the key in [distanceVector] and
+# each value is a dictionary containing two keys 'nextHop' and 'cost'.
+# The value to nextHop is initialized to be the string name of the key
+# in [distanceVector].  The value to cost is initialized to be the
+# values in [distanceVector].
 def initializeRoutingTable(distanceVector):
 	routingTable = {}
 	routingTable['host'] = distanceVector['host']
@@ -54,11 +71,32 @@ def initializeRoutingTable(distanceVector):
 			routingTable[distanceVectorHost] = {'nextHop':distanceVectorHost, 'cost':distanceVectorCost}
 	return routingTable
 
+# udpClient opens a udpConnection with [serverName]:[serverPort].
+#
+# input arguments:
+# 1. serverName - the string name of the server to which you would like
+# to open a UDP connection.
+# 2. serverPort - the integer port number of the server to which you would
+# like to open a UDP connection.
 def udpClient(serverName, serverPort):
 	clientSocket = socket(AF_INET, SOCK_DGRAM)
 	clientSocket.sendto(str(distanceVector),(serverName, serverPort))
 	clientSocket.close()
 
+# a clientThread is a three tuple consisting of a port number,
+# a sequence number that you would like the print statements to start at
+# and the number of seconds you would like to wait between print
+# statements.  Every [sleepSeconds] seconds, [distanceVector] is sent
+# to all hosts in [distanceVector] via UDP and [routingTable] is updated
+# accordingly.
+#
+# input arguments:
+# 1. port - the integer port number that you would like to open a
+# udp connection on.
+# 2. sequenceNumber - the integer that you would like the sequence
+# in the print statements to start on (1 is highly suggested).
+# 3. sleepSeconds - the integer number of seconds you would like
+# between udp messages to all hosts in [distanceVector].
 class clientThread(threading.Thread):
 	def __init__(self, port, sequenceNumber, sleepSeconds):
 		threading.Thread.__init__(self)
@@ -79,7 +117,16 @@ class clientThread(threading.Thread):
 					print 'shortest path to node '+key+': the next hop is '+value['nextHop']+' and the cost is '+str(value['cost'])
 			time.sleep(self.sleepSeconds)
 			self.sequenceNumber += 1
-			
+
+# updateRoutingTable updates [routingTable] based on information
+# recieved from UDP connections.  [routingTable] is updated if
+# a better route is found to any hosts in [routingTable].
+#
+# input arguments:
+# 1. distanceVectorDictionaryReceived - a dictionary representing
+# a distance vector recieved from a directly connected client.
+# [routingTable] is updated if a directly connected client knows
+# of a more efficient route to any host in [routingTable].
 def updateRoutingTable(distanceVectorDictionaryReceived):
 	# hostRecieved will hold the host from which this distance vector was sent
 	hostRecieved = distanceVectorDictionaryReceived['host']
@@ -102,6 +149,14 @@ def updateRoutingTable(distanceVectorDictionaryReceived):
 					newNextHop = routingTable[hostRecieved]['nextHop']
 					routingTable[distanceVectorHostRecieved] = {'nextHop':newNextHop, 'cost':newCost}
 
+# a serverThread is a one-tuple consisting of an integer port number.
+# serverThread will listen on this port number for incomming UDP connections.
+# serverThread will update [routingTable] if a more efficient route is found
+# in any client's [distanceVector].
+#
+# input arguments:
+# 1. port - an integer representing the port number to listen for incomming UDP
+# connections.
 class serverThread(threading.Thread):
 	def __init__(self, port):
 		threading.Thread.__init__(self)
@@ -120,6 +175,7 @@ distanceVector = computeDistanceVector(dataFileLocation)
 # routingTable is initialized with information from distanceVector
 routingTable = initializeRoutingTable(distanceVector)
 
+# main method spawns off client thread and server thread.
 def main():
 	if len(sys.argv) == 3:
 		port = int(sys.argv[2])
